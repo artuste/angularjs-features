55ec6dd28f8e13afff7a1c32e2a190c95ef0ce9f
astepien <artuste@gmail.com>
2014-07-29 12:57:01 +0200

    Minor.

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1>Base AngularJS Application</h1>
+
+<p>In command-line go to the project folder.</p>
+
+<h2>Install npm</h2>
+<pre>npm install</pre>
+
+<h2>Install bower</h2>
+<pre>bower install</pre>
+
+<h2>Grunt wiredep plugin</h2>
+<pre>grunt wiredep</pre>
---
+++ karma.conf.js
@@ -0,0 +1,84 @@
+// Karma configuration
+// Generated on Thu Jul 03 2014 10:33:09 GMT+0200 (Central European Daylight Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        'bower_components/angular/angular.js',
+        'bower_components/angular-mocks/angular-mocks.js',
+        'bower_components/angular-resource/angular-resource.js',
+        'bower_components/angular-translate/angular-translate.js',
+        'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
+        'bower_components/angular-translate-loader-url/angular-translate-loader-url.js',
+        'app/modules/*/app.js',
+        'app/modules/*/*/*/*.js',
+        'app/modules/*/*/*/*/*.js',
+        'app/tests/*Spec.js',
+        'app/tests/*/*Spec.js'
+    ],
+      
+    plugins : [
+        'karma-phantomjs-launcher',
+        'karma-jasmine',
+        'karma-chrome-launcher',
+        'karma-firefox-launcher'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
+  });
+};
