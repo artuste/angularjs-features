b56f7a5b48d0506056adcf0c92a4f4938d2db4f0
astepien <artuste@gmail.com>
2014-07-31 15:08:23 +0200

    Minor.

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -4,3 +4,57 @@ $(function () {
     UIGeneral.init();
 });
 
+//$script('bower_components/jquery/dist/jquery.js', 'jquery');
+//$script('bower_components/bootstrap/dist/js/bootstrap.js', 'bootstrap');
+//$script('bower_components/angular/angular.js', 'angular');
+//$script('app/assets/theme/global/scripts/metronic.js', 'metronic');
+//
+//$script
+//    .ready('angular', function () {
+//        $script([
+//            'bower_components/angular-route/angular-route.js',
+//            'bower_components/angular-mocks/angular-mocks.js',
+//            'bower_components/angular-resource/angular-resource.js',
+//            'bower_components/angular-local-storage/angular-local-storage.js',
+//            'bower_components/angular-ui-router/release/angular-ui-router.js',
+//            'bower_components/angular-cookies/angular-cookies.js',
+//            'bower_components/angular-translate/angular-translate.js',
+//            'bower_components/angular-translate-loader-url/angular-translate-loader-url.js',
+//            'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js'
+//        ]);
+//    })
+//    .ready('jquery', function () {
+//        $script([
+//            'bower_components/jquery-ui/jquery-ui.js',
+//            'app/assets/theme/global/plugins/jquery.blockui.min.js',
+//            'app/assets/theme/global/plugins/jquery.cokie.min.js',
+//            'app/assets/theme/global/plugins/uniform/jquery.uniform.min.js',
+//            'app/assets/theme/global/plugins/jquery.pulsate.min.js',
+//            'app/assets/theme/global/plugins/jquery-bootpag/jquery.bootpag.min.js',
+//            'app/assets/theme/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js'
+//        ]);
+//    })
+//    .ready('bootstrap', function () {
+//        $script([
+//            'bower_components/bootstrap-switch/dist/js/bootstrap-switch.js',
+//            'app/assets/theme/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js'
+//        ]);
+//    })
+//    .ready('metronic', function () {
+//        $script([
+//            'app/assets/theme/admin/layout/scripts/layout.js',
+//            'app/assets/theme/admin/layout/scripts/quick-sidebar.js',
+//            'app/assets/theme/admin/pages/scripts/ui-general.js',
+//            'app/assets/theme/global/plugins/holder.js'
+//        ]);
+//    })
+//    .ready('angular', function () {
+//        $script([
+//            'app/modules/admin/app.js',
+//            'app/modules/admin/home/controllers/controllers.home.js'
+//        ], function () {
+//            Metronic.init();
+//            Layout.init();
+//            UIGeneral.init();
+//        });
+//    });
