ba74476b1318cbb17c32b99d1378d953233dab79
artuste <artuste@gmail.com>
2015-03-19 19:39:45 +0100

    Menu added.

---
+++ app/modules/menu/menu.js
@@ -0,0 +1,22 @@
+(function () {
+    "use strict";
+
+    angular.module('app')
+        .controller('MenuCtrl', MenuCtrl);
+
+    MenuCtrl.$inject = ['$location'];
+
+    function MenuCtrl($location) {
+        /*jshint validthis: true */
+        var vm = this;
+
+        vm.getPath = function (path) {
+            if ($location.path().substr(0, path.length) == path) {
+                return "active";
+            } else {
+                return "";
+            }
+        }
+    }
+
+})();
